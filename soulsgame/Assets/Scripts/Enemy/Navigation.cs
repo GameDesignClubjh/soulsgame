23872908c7cd74cf7f7e60594fd7553cab6db72d
noknight12 <karlonasa12@gmail.com>
2024-11-03 22:41:44 -0800

    started working on the Navigation script
    added functions to send a ray towards the target object to navigate to

--- soulsgame/Assets/Scripts/Enemy/Navigation.cs
+++ soulsgame/Assets/Scripts/Enemy/Navigation.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
+
+    bool playerFound = false;
+
+    [SerializeField]
+    float fov = 0.0f;
+    Transform target;
+    float wallAvoidLength;
+    TagHandle wall;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +22,29 @@ public class Navigation : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SendRay();
+       
+    }
+
+    void SendRay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position);
+        if (hit.collider.tag == target.tag)
+        {
+            playerFound = true;
+            InvokeRepeating("SendRay", 0.0f, 0.3f);
+        }
+    }
+
+
+
+    private void findPath()
+    {
+        List<Vector2> firstList = new List<Vector2>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position);
+    }
+    
 }
