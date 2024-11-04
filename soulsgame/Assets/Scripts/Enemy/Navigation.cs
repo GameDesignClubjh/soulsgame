f9c84b13bb3ddd30a86e19b0389ce86a89390371
noknight12 <karlonasa12@gmail.com>
2024-11-04 00:03:12 -0800

    added comments
    added comments to navigation script

--- soulsgame/Assets/Scripts/Enemy/Navigation.cs
+++ soulsgame/Assets/Scripts/Enemy/Navigation.cs
@@ -7,10 +7,11 @@ public class Navigation : MonoBehaviour
     bool playerFound = false;
 
     [SerializeField]
-    float fov = 0.0f;
-    Transform target;
-    float wallAvoidLength;
-    TagHandle wall;
+    float fov = 0.0f;   //idk if I'm using this yet
+    [SerializeField]
+    Transform target;  //the target
+    [SerializeField]
+    string wallTag; //tag of the obstacles
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,9 +30,9 @@ public class Navigation : MonoBehaviour
        
     }
 
-    void SendRay()
+    void SendRay() //sends a raycast to the target to see if there is anything in thhe way
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position); //sends a ray to the target
         if (hit.collider.tag == target.tag)
         {
             playerFound = true;
@@ -41,10 +42,18 @@ public class Navigation : MonoBehaviour
 
 
 
-    private void findPath()
+    private void findPath() //finds the path to the target
     {
-        List<Vector2> firstList = new List<Vector2>();
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position);
+        Queue<Vector2> checkpoints = new Queue<Vector2>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, target.position);  //send a ray to the target
+        if(hit.collider.tag == wallTag) 
+        {
+
+           
+            checkpoints.Enqueue(hit.transform.position);
+            RaycastHit2D sweep = Physics2D.Raycast(transform.position, hit.transform.position);  //sends a ray to the collision
+            // make the ray sweep left and right until it sees open space
+        }
     }
     
 }
